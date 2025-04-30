216ae39a5cd94060f08bedae072a8ead6a24e278
herrdorrsnorre <77855285+herrdorrsnorre@users.noreply.github.com>
2025-04-30 12:41:06 -0400

    changed speed

--- Apple.java
+++ Apple.java
@@ -15,7 +15,7 @@ public class Apple extends Actor
     public void act()
     {
         // Add your action code here.
-        setLocation(getX(), getY()+3);
+        setLocation(getX(), getY()+2);
         MyWorld world = (MyWorld) getWorld();
         if(getY() >= world.getHeight()) 
         {
