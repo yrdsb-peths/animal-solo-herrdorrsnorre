22fbb31fad9e1ea631b94703d21a3c39a7d1ed54
herrdorrsnorre <77855285+herrdorrsnorre@users.noreply.github.com>
2025-05-09 09:10:13 -0400

    Added level

--- MyWorld.java
+++ MyWorld.java
@@ -3,6 +3,7 @@ import greenfoot.*;
 public class MyWorld extends World {
     public int score = 0;
     Label scoreLabel;
+    int level = 1;
     public MyWorld() {
         super(640, 359, 1, false);
         
@@ -25,11 +26,16 @@ public class MyWorld extends World {
     {
         score++;
         scoreLabel.setValue(score);
+        
+        if(score % 5 == 0) {
+            level += 1;
+        }
     }
     
     public void createApple()
     {
         Apple apple = new Apple();
+        apple.setSpeed(level);
         int x = Greenfoot.getRandomNumber(600);
         int y = Greenfoot.getRandomNumber(50);
         addObject(apple, x, y);
--- TitleScreen.java
+++ TitleScreen.java
@@ -16,9 +16,9 @@ public class TitleScreen extends World
     public TitleScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(612, 388, 1); 
 
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+        addObject(titleLabel, getWidth()/2, 100);
         prepare();
     }
 
@@ -30,6 +30,7 @@ public class TitleScreen extends World
             Greenfoot.setWorld(gameWorld);
         }
     }
+    
     /**
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
@@ -38,5 +39,27 @@ public class TitleScreen extends World
     {
         Elephant elephant = new Elephant();
         addObject(elephant,485,72);
+        elephant.setLocation(503,68);
+        elephant.setLocation(516,86);
+        Label label = new Label("Use <-- and --> to Move", 50);
+        addObject(label,259,296);
+        label.setLocation(272,229);
+        label.setLocation(273,229);
+        Label label2 = new Label("Press <space> to Start", 50);
+        addObject(label2,241,340);
+        label2.setLocation(284,336);
+        label2.setLocation(342,325);
+        label2.setLocation(263,319);
+        label.setLocation(367,228);
+        elephant.setLocation(528,81);
+        label.setLocation(358,196);
+        label.setLocation(301,184);
+        label2.setLocation(275,294);
+        label.setLocation(305,211);
+        label.setLocation(274,220);
+        elephant.setLocation(514,72);
+        elephant.setLocation(550,81);
+        elephant.setLocation(311,24);
+        elephant.setLocation(268,169);
     }
 }
